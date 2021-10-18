b332208b4d14a7845c2544ba8cec70fdef1dfd2c
Mark Fosseddy <markfosseddy@gmail.com>
2021-10-18 11:22:19 +0500

    add examples folder

---
+++ assembler/examples/add.asm
@@ -0,0 +1,12 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/add/Add.asm
+
+
+@2           			//commen
+D=A
+@3    // To this is comment
+					D=D+A                                // comt 
+@R0
+   M=D
