f7030c277c8ba398839227fc53f7d16bae5eacb8
Mark Fosseddy <markfosseddy@gmail.com>
2021-10-19 20:05:49 +0500

    refactro op translation into separate module

--- virtual-machine-translator/vm-translator.c
+++ virtual-machine-translator/vm-translator.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "parser.h"
+#include "translator.h"
 
 #define OUTPUT_EXT ".asm"
 
@@ -21,26 +22,7 @@ int main (int argc, char **argv)
     FILE *f_out = fopen(out_file, "w");
     while (parser_peek_line(&p)) {
         Instruction inst = parser_parse_instruction(&p);
-        printf("%d | ", inst.op_type);
-        printf("%d | ", inst.mem_seg_type);
-        printf("%ld\n", inst.mem_addr);
-
-        switch (inst.op_type) {
-            case OP_TYPE_PUSH: {
-                fprintf(f_out, "// PUSH OP\n");
-                fprintf(f_out, "@%li\n", inst.mem_addr);
-                fprintf(f_out, "D=A\n");
-                fprintf(f_out, "@SP\n");
-                fprintf(f_out, "A=M\n");
-                fprintf(f_out, "M=D\n");
-                fprintf(f_out, "@SP\n");
-                fprintf(f_out, "M=M+1\n");
-            } break;
-
-            case OP_TYPE_ADD: {
-                fprintf(f_out, "// ADD OP\n");
-            } break;
-        }
+        translator_translate_inst(&inst, f_out);
     }
 
     fclose(f_out);
