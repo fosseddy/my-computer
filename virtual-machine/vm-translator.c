627736fa274c30acebacd5109fa562fd79636c58
Mark Fosseddy <markfosseddy@gmail.com>
2021-10-21 03:01:22 +0500

    bug fix with out_file name

--- virtual-machine/vm-translator.c
+++ virtual-machine/vm-translator.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
+#include <stdlib.h>
 #include "parser.h"
 #include "translator.h"
 
 #define OUTPUT_EXT ".asm"
+#define PATH_SEP '/'
 
 void create_out_file_from_in_file(const char *in_file, char *out_file);
-
-// @TODO: change out_path using argv[0]
 
 int main (int argc, char **argv)
 {
     assert(argc > 1);
 
     const char *in_file = argv[1];
-    char out_file[strlen(in_file) + 2];
+    char out_file[256];
     create_out_file_from_in_file(in_file, out_file);
 
     Parser p = make_parser(in_file);
@@ -34,8 +33,18 @@ int main (int argc, char **argv)
 
 void create_out_file_from_in_file(const char *in_file, char *out_file)
 {
-    size_t i = 0;
-    for (; in_file[i] != '.'; ++i) out_file[i] = in_file[i];
-    out_file[i] = '\0';
+    int dot_pos = strlen(in_file) - 3;
+    int len = 0;
+    for (int i = dot_pos - 1; i >= 0 && in_file[i] != PATH_SEP; --i, ++len) {
+        out_file[len] = in_file[i];
+    }
+    out_file[len] = '\0';
+
+    for (int i = 0; i < (len / 2); ++i) {
+        int tmp = out_file[i];
+        out_file[i] = out_file[len - 1 - i];
+        out_file[len - 1 - i] = tmp;
+    }
+
     strcat(out_file, OUTPUT_EXT);
 }
