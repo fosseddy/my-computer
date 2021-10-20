ae7fadbd8ac0081084d06078c60ecb02943e4181
Mark Fosseddy <markfosseddy@gmail.com>
2021-10-21 03:37:56 +0500

    add path modules

---
+++ virtual-machine/path.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <assert.h>
+
+#define PATH_SEP '/'
+
+void path_get_file_name(char *out, const char *s)
+{
+    int dot_pos = strlen(s) - 1;
+    for (; s[dot_pos] != '.'; --dot_pos);
+
+    int len = 0;
+    for (int i = dot_pos - 1; i >= 0 && s[i] != PATH_SEP; --i, ++len) {
+        out[len] = s[i];
+    }
+    out[len] = '\0';
+
+    for (int i = 0; i < (len / 2); ++i) {
+        int tmp = out[i];
+        out[i] = out[len - 1 - i];
+        out[len - 1 - i] = tmp;
+    }
+}
+
+void path_concat_ext(char *s, const char *ext)
+{
+    strcat(s, ext);
+}
---
+++ virtual-machine/path.h
@@ -0,0 +1,7 @@
+#ifndef PATH_H
+#define PATH_H
+
+void path_get_file_name(char *out, const char *s);
+void path_concat_ext(char *s, const char *ext);
+
+#endif
