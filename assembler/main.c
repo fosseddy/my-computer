043e0ab1f21e0c608d5289bd08353cde39e2dd73
fosseddy <markfosseddy@gmail.com>
2023-03-13 00:11:34 +0500

    read file

--- assembler/main.c
+++ assembler/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 void print_usage(FILE *stream, char *msg)
 {
@@ -15,10 +16,38 @@ void print_usage(FILE *stream, char *msg)
                     "   path to assembly file\n");
 }
 
+char *read_file(char *filepath)
+{
+    FILE *f;
+    char *src;
+    struct stat statbuf;
+
+    f = fopen(filepath, "r");
+    if (f == NULL) return NULL;
+
+    if (stat(filepath, &statbuf) < 0) return NULL;
+
+    src = malloc(statbuf.st_size);
+    if (src == NULL) return NULL;
+
+    fread(src, 1, statbuf.st_size, f);
+    return src;
+}
+
+struct instruction {
+    void *ptr;
+};
+
+int parse_instruction(struct instruction *inst)
+{
+    (void) inst;
+    return 1;
+}
+
 int main(int argc, char **argv)
 {
-    FILE *infile;
-    struct stat statbuf;
+    char *src;
+    struct instruction inst;
 
     --argc;
     ++argv;
@@ -28,18 +57,15 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    infile = fopen(*argv, "r");
-    if (infile == NULL) {
-        perror("failed to open file");
+    src = read_file(*argv);
+    if (src == NULL) {
+        perror("failed to read file");
         return 1;
     }
 
-    if (stat(*argv, &statbuf) < 0) {
-        perror("failed to get info about file");
-        return 1;
+    for (;;) {
+        if (parse_instruction(&inst) == 0) break;
     }
-
-    printf("file size is %d\n", statbuf.st_size);
 
     return 0;
 }
